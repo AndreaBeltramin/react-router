0c49898c402a1eda1da86984876a5e5c8e9ed0a2
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-11 16:41:18 +0100

    fix fetch index

--- src/pages/posts/IndexPosts.jsx
+++ src/pages/posts/IndexPosts.jsx
@@ -3,45 +3,45 @@ import { useEffect, useState } from "react";
 
 export default function IndexPosts() {
 	const apiUrl = import.meta.env.VITE_API_URL;
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		fetchIndexPosts();
+	}, []);
 
-	fetch(apiUrl + "/posts")
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-		});
-
+	const fetchIndexPosts = () => {
+		fetch(apiUrl + "/posts")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setPosts(data);
+			});
+	};
 	return (
 		<>
 			<div className="container mt-5">
 				<h1>Posts</h1>
-				<div className="row row-cols-4 g-3 mt-3 mb-5">
-					<div className="col">
-						<div className="card">
-							<img
-								src="http://localhost:3000/images/ciambellone.jpeg"
-								className="card-img-top"
-								alt="..."
-							/>
-							<div className="card-body">
-								<h5 className="card-title">post.title</h5>
-								<p className="card-text">post.content</p>
-								<a href="#" className="btn btn-primary">
-									Scopri di più
-								</a>
+				{posts.length > 0 ? (
+					<div className="row row-cols-3 g-3 mt-3 mb-5">
+						{posts.map((post) => (
+							<div className="col" key={post.id}>
+								<div className="card">
+									<img src={post.img} className="card-img-top" alt="..." />
+									<div className="card-body">
+										<h5 className="card-title">{post.title}</h5>
+										<p className="card-text">{post.content}</p>
+										<a href="#" className="btn btn-primary">
+											Scopri di più
+										</a>
+									</div>
+								</div>
 							</div>
-						</div>
+						))}
 					</div>
-				</div>
+				) : (
+					<h2>Nessun post da visualizzare</h2>
+				)}
 
 				<div className="row row-cols-4 g-3 mb-5">
-					<PostCard />
-					<PostCard />
-					<PostCard />
-					<PostCard />
-					<PostCard />
-					<PostCard />
-					<PostCard />
-					<PostCard />
 					<PostCard />
 					<PostCard />
 					<PostCard />
